37ef37abb58499280d69df616dfc33c831261ff4
mrizkiprmn <rizkiypermana@gmail.com>
2022-01-22 03:52:27 +0700

    add helm chart postgresql,redis & changes redis.js config

--- src/Configs/redis.js
+++ src/Configs/redis.js
@@ -6,6 +6,7 @@ class Redis {
         this.redisdb = redis.createClient({
             host: process.env.REDIS_HOST,
             port: process.env.REDIS_PORT,
+            password: process.env.REDIS_PASS,
         })
     }
 
